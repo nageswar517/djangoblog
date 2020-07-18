4797c219fd22119a6a7b35b3c18f0f75aa7c58b6
First Last <nageswar.dvsg@gmail.com>
2020-07-18 17:21:19 +0530

    static

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd*gs^pgg4iz_)oiojy_ui3uug_raww#s$u=0p&y%as%ky@3bm#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
